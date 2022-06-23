eddd648285ac0716ba02910b3f80e1ee78964364
lucysommy <lucianachisom@gmail.com>
2022-06-23 23:34:36 +0100

    first commit

---
+++ index.html/introduction to javascript/java.js
@@ -0,0 +1,8 @@
+let user = { Name: 'Iwuanyanwu Chisom',
+country:' Nigeria',
+Height: '1.65'
+}
+
+document.write(user.Name + '<br>' + '<br>')
+document.write(user.country + '<br>' + '<br>')
+document.write(user.Height)
